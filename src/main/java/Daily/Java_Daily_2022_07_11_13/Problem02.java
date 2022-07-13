5462f626636428bc3b6b58e65687e7c4e8176569
Jason Cameron <dev.jasoncameron@gmail.com>
2022-07-13 11:44:03 -0500

    comp prob 02

---
+++ src/main/java/Daily/Java_Daily_2022_07_11_13/Problem02.java
@@ -0,0 +1,62 @@
+/**     Problem 02
+ *
+ *
+ *      You are given an integer , you have to convert it into a string.
+ *
+ *      Please complete the partially completed code in the editor. If your
+ *      code successfully converts  into a string  the code will print "Good job".
+ *      Otherwise, it will print "Wrong answer".
+ *
+ *      can range between -100  to 100 inclusive.
+ *
+ *
+ */
+package Daily.Java_Daily_2022_07_11_13;
+
+import java.util.*;
+import java.security.*;
+public class Problem02 {
+    public static void main(String[] args) {
+
+        DoNotTerminate.forbidExit();
+
+        try {
+            Scanner in = new Scanner(System.in);
+            int n = in .nextInt();
+            in.close();
+
+
+            String s = String.valueOf(n);               //   This all they were looking for
+
+
+            if (n == Integer.parseInt(s)) {
+                System.out.println("Good job");
+            } else {
+                System.out.println("Wrong answer.");
+            }
+        } catch (DoNotTerminate.ExitTrappedException e) {
+            System.out.println("Unsuccessful Termination!!");
+        }
+    }
+}
+
+//The following class will prevent you from terminating the code using exit(0)!
+class DoNotTerminate {
+
+    public static class ExitTrappedException extends SecurityException {
+
+        private static final long serialVersionUID = 1;
+    }
+
+    public static void forbidExit() {
+        final SecurityManager securityManager = new SecurityManager() {
+            @Override
+            public void checkPermission(Permission permission) {
+                if (permission.getName().contains("exitVM")) {
+                    throw new ExitTrappedException();
+                }
+            }
+        };
+        System.setSecurityManager(securityManager);
+    }
+}  //<--END
