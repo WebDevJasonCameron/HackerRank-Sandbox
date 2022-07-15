9b86140866c40f0a896f12f01c83a8c5c7dc4696
Jason Cameron <dev.jasoncameron@gmail.com>
2022-07-15 13:51:15 -0500

    prob one comp, with three parts. skill: ez

---
+++ src/main/java/Daily/Year_2022/Week_28/Java_Daily_2022_07_11_15/Init_Daily.java
@@ -0,0 +1,16 @@
+package Daily.Year_2022.Week_28.Java_Daily_2022_07_11_15;
+
+public class Init_Daily {
+
+    public static void main(String[] args) {
+
+        Problem01 problem01 = new Problem01();
+
+        problem01.sumOfStrings("abc", "b");
+        problem01.compareStringValue("hello", "java");
+        problem01.combineStrings("hello", "java");
+
+    }
+
+
+} //<--END
