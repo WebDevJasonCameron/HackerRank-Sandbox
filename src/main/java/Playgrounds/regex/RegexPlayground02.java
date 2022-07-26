998fd94ca692f6d997b0e845e7dd5ec168a473a2
Jason Cameron <dev.jasoncameron@gmail.com>
2022-07-26 13:15:15 -0500

    txt files to practice regex added

---
+++ src/main/java/Playgrounds/regex/RegexPlayground02.java
@@ -0,0 +1,9 @@
+/**
+ *      Lesson From
+ *      https://www.youtube.com/watch?v=sa-TUpSx1JA
+ */
+
+package Playgrounds.regex;
+
+public class RegexPlayground02 {
+}
--- src/main/java/Playgrounds/regex/init.java
+++ src/main/java/Playgrounds/regex/init.java
@@ -1,44 +1,8 @@
-/**
- *      Code for this copied from:
- *      https://www.youtube.com/watch?v=vuv5oAfRoQA
- */
-
 package Playgrounds.regex;
-
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class init {
 
     // RUN
     public static void main(String[] args) {
-
-        displayFind("(Lizard)[sz]","Should I name my new pet store Happy Lizards or Happy Lizardz?");
-        displayFind("(lizard)[sz]","Is a snake a lizard?");
-        displayFind("Lizard[sz]","Should I name my new pet store Happy Lizards or Happy Lizardz?");
-        displayFind("(Lizards)|(Lizardz)","Should I name my new pet store Happy Lizards or Happy Lizardz?");
-        displayFind("(Lizard)s|(Lizard)z","Should I name my new pet store Happy Lizards or Happy Lizardz?");
-        displayFind("(Lizard)[s]|(Lizard)[z]","Should I name my new pet store Happy Lizards or Happy Lizardz?");
-        displayFind("((Lizard)[s])|((Lizard)[z])","Should I name my new pet store Happy Lizards or Happy Lizardz?");
-        displayFind("(Lizard)[sz]|(my)","Should I name my new pet store Happy Lizards or Happy Lizardz?");
-
-    }
-
-    // METH
-    static void displayFind(String regex, String searchMe){
-        boolean foundIt = false;
-//        Pattern p = Pattern.compile(regex);
-//        Matcher m = p.matcher(searchMe);
-        Matcher m = Pattern.compile(regex).matcher(searchMe);
-
-        while ((m.find())) {
-            System.out.println("Matcher found " + m.group() + "at index " + m.start() + " for regex " + regex + " in string \"" + searchMe + "\"");
-            foundIt = true;
-        }
-        if(!foundIt){
-            System.out.println("No matches found for " + regex + " in string \"" + searchMe + "\'");
-        }
-        System.out.println("");
 
     }
 
